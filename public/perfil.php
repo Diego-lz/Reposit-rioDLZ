8452e299e599c122e520bc7dce7621138271956b
Diego-lz <272330@unifio.edu.br>
2025-09-03 23:08:27 +0000

    P\\Thiago

---
+++ public/perfil.php
@@ -0,0 +1,10 @@
+<h1>Perfil do Usuário</h1>
+
+<?php $nome = "Diego"; ?>
+<p>Bem vindo, <?php echo $nome; ?>!</p>
+
+<?php $idade = "19"; ?>
+<p>idade: <?php echo $idade; ?> anos</p>
+
+<?php $cidade = "Palmital"; ?>
+<p>cidade: <?php echo $cidade; ?> </p>
